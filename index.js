a04c2f3efed51896ea71f0add6d157f50b5d3b13
jstreich13 <jstreich13@gmail.com>
2022-06-28 15:59:13 -0500

    Initial CoOmmit

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.port || 8080;
-const userRoutes = require('./routes/users');
+const userRoutes = require("./routes/users");
 
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use('/api/users', userRoutes);
+app.get("/", (req, res) => {
+  res.send(`<h1>Welcome to my Express App</h1>`);
+});
 
+// Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });

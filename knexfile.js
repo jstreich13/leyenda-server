ea43cb4b4aaefb1ee5716a2abc277bd97e8a638d
jstreich13 <jstreich13@gmail.com>
2022-07-07 16:39:29 -0600

    heroku bugging

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,20 @@
-module.exports = {
-  client: "mysql",
-  connection: {
-    host: "127.0.0.1",
-    user: "root",
-    password: "rootroot",
-    database: "leyenda_data",
-    charset: "utf8",
+const connections = {
+  development: {
+    client: "mysql",
+    connection: {
+      host: "127.0.0.1",
+      user: "root",
+      password: "rootroot",
+      database: "leyenda_data",
+    },
+  },
+  production: {
+    client: "mysql",
+    connection: process.env.JAWSDB_URL,
   },
 };
+
+module.exports =
+  process.env.NODE_ENV === "production"
+    ? connections.production
+    : connections.development;
